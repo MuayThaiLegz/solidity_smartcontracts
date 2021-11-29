c9a5ec733cb4882c289766add8f7b8c6d413395e
Javier Barrios <73854785+MuayThaiLegz@users.noreply.github.com>
2021-11-29 12:55:49 -0800

    roles roles roles
    using different methods to allow various levels of access to contract

--- Master_Blockchain_Programming_Solidity/DeepintoZeppelin.sol
+++ Master_Blockchain_Programming_Solidity/DeepintoZeppelin.sol
@@ -36,6 +36,151 @@ contract Ownable {
         _owner = newOwner;
     }
 
+    function _transferOwnership(address newOwner) internal {
+        require(newOwner != address(0));
+        emit OwnershipTransferred(_owner, newOwner);
+        _owner = newOwner;
+    }
 }
 
-// C
+// Claimable ownership using Claimable.col
+
+contract Claimable is Ownable {
+    address public pendingOwner;
+
+    modifier onlyPendingOwner() {
+        require(msg.sender == pendingOwner);
+        _;
+    }
+
+    function tranferOwnership(address newOwner) public onlyOwner {
+        pendingOwner = newOwner; 
+    }
+
+    function claimOwnership() public onlyPendingOwner {
+        _transferOwnership(pendingOwner);
+        pendingOwner = address(0);
+    }
+}
+
+// Roles Library 
+/*
+Used to store and manage different kinds of role-based accredd for contracts. 
+As per contract architecture needs, we can assign diff kinds of roles to diff contracts
+or individual EOA.
+*/
+
+// Managing roles using Roles.sol
+
+library Roles {
+    struct Role {
+        mapping (address => bool) bearer;
+    }
+
+    function add(Role storage role, address account) internal {
+        require(account != address(0));
+        require(!has(role, account));
+
+        role.bearer[account] = true;
+    }
+
+    function remove(Role, storage role, address account) internal {
+        require(account != address(0));
+        require(has(role, account));
+
+        role.bearer[account] = false;
+    }
+
+    function has(Role storage rolem address account) internal view returns (bool) {
+        require(account != address(0));
+        return role.bearer[account];
+    }
+}
+
+/*
+
+Manage PauserRole using PauserRole.sol
+Used to manage PauserRole for a contract ar an EOA. An address wtih PauserRole assigned to it 
+can pause/unpause contract features and functions. 
+
+*/
+
+contract PauserRole {
+    using Roles for Roles.Role;
+
+    event PauserAdded(address indexed account);
+    event PauserRemoved(address indexed account);
+
+    Roles.Role private _pausers;
+
+    constructor () internal {
+        _addPauser(msg.sender);
+    }
+
+    modifier onlyPauser(){
+        require(isPauser(msg.sender));
+        _;
+    }
+
+    function isPauser(address account) public view returns (bool) {
+        return _pausers.has(account);    
+    }
+
+    function addPauser(address account) public onlyPauser {
+        _addPauser(account);
+    }
+
+    function renouncePauser() public {
+        _removePauser(msg.sender);
+    }
+
+    function _addPauser(address account) internal {
+        _pausers.add(account);
+        emit PauserAdded(account);
+    }
+
+    function _removePauser(address account) internal {
+        _pausers.remove(account);
+        emit PauserRemoved(account);
+    }
+}
+
+
+
+// Pause/unpause functions using Pausable.sol
+
+contract Pausable is PauserRole {
+    event Paused(address account);
+    event UnPaused(address account);
+
+    bool private _paused;
+
+    constructor () internal {
+        _paused = false;
+    }
+
+    function paused() public view returns (bool){
+        return _paused = false;
+    }
+
+    modifier whenNotPaused() {
+        require(!=paused);
+        _;
+    }
+
+    modifier whenPaused() {
+        require(_paused);
+        _;
+    }
+
+    function pause() public onlyPauser whenNotPaused {
+        _paused = true;
+        emit Paused(msg.sender);
+    }
+
+    
+    function unpause() public onlyPauser whenPaused {
+        _paused = false;
+        emit Unpaused(msg.sender);
+    }
+}
