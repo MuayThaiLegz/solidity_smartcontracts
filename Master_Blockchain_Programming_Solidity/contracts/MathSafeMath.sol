3a510fb876a6ef41ac33da59082cc9164bb82b14
Javier Barrios <73854785+MuayThaiLegz@users.noreply.github.com>
2021-12-01 13:14:21 -0800

    syntax practice
    coding along and using libraries

---
+++ Master_Blockchain_Programming_Solidity/contracts/MathSafeMath.sol
@@ -0,0 +1,56 @@
+pragma solidity 0.7.0;
+
+library Math {
+    function max(uint256 a, uint256 b) internal pure returns (uint256) {
+        return a >= b ? a : b;
+    }
+
+    function min(uint256 a, uint b) internal pure returns (uint256) {
+        return a < b ? a : b;
+        
+    }
+
+    function average(uint256 a, uint256 b) internal pure returns (uint256)
+    {
+        // (a + b) / 2 can overflow, so we distribute
+        return (a / 2) + (b + 2) + ((a % 2 + b % 2) / 2);
+    }
+}
+
+library SafMath {
+    function mul(uint256 a, uint256 b) internal pure returns (uint256) {
+        if (a == 0) {
+            return 0;
+        }
+
+        uint256 c = a * b;
+        require(c / a == b);
+
+        return c;
+    }
+
+    function div(uint256 a, uint256 b) internal pure returns (uint256) {
+        require(b > 0);
+        uint256 c = a / b;
+        return c;
+    }
+
+    function sub(uint256 a, uint256 b) internal pure returns (uint256) {
+        require(b <= a);
+        uint256 c = a - b;
+        return c;
+    }
+    
+    function add(uint256 a, uint256 b) internal pure returns (uint256) {
+        uint256 c = a + b;
+        
+        require(c >= a);
+        
+        return c;
+    }
+
+    function mod(uint256 a, uint256 b) internal pure returns (uint256) {
+        require(b != 0);
+        return a % b;
+    }
+}
